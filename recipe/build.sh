f3f453308b625ab00096540702251f0cb39ce8ee
danielnachun <daniel.nachun@gmail.com>
2023-10-30 13:59:17 -0700

    fix build and add new targets

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,10 +7,17 @@ cargo-bundle-licenses \
     --output THIRDPARTY.yml
 
 export SODIUM_USE_PKG_CONFIG=1
-cargo install --locked --root "${PREFIX}" --path .
+cargo install --locked --root "${PREFIX}" --path atuin
 
 # strip debug symbols
 "${STRIP}" "${PREFIX}/bin/atuin"
+
+mkdir -p ${PREFIX}/etc/bash_completion.d/atuin
+mkdir -p ${PREFIX}/share/zsh/site-functions/_atuin
+mkdir -p ${PREFIX}/share/fish/vendor_completions.d/atuin.fish
+atuin gen-completion --shell bash --out-dir ${PREFIX}/etc/bash_completion.d/atuin
+atuin gen-completion --shell zsh --out-dir ${PREFIX}/share/zsh/site-functions/_atuin
+atuin gen-completion --shell fish --out-dir ${PREFIX}/share/fish/vendor_completions.d/atuin.fish
 
 # remove extra build file
 rm -f "${PREFIX}/.crates.toml"
