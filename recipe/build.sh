8f8cd5c339346030fbdcc5a1a17faeb9ae79fbea
danielnachun <daniel.nachun@gmail.com>
2023-10-30 14:56:25 -0700

    fix cross compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,19 +11,14 @@ cargo install --locked --root "${PREFIX}" --path atuin
 # strip debug symbols
 "${STRIP}" "${PREFIX}/bin/atuin"
 
-# Use existing version of atuin for completion generation on cross-compiled target.
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
-    atuin_path="${BUILD_PREFIX}/bin/atuin"
-else
-    atuin_path="${PREFIX}/bin/atuin"
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+    mkdir -p ${PREFIX}/etc/bash_completion.d/${PKG_NAME}
+    mkdir -p ${PREFIX}/share/zsh/site-functions/_${PKG_NAME}
+    mkdir -p ${PREFIX}/share/fish/vendor_completions.d/${PKG_NAME}.fish
+    ${PKG_NAME} gen-completion --shell bash --out-dir ${PREFIX}/etc/bash_completion.d/${PKG_NAME}
+    ${PKG_NAME} gen-completion --shell zsh --out-dir ${PREFIX}/share/zsh/site-functions/_${PKG_NAME}
+    ${PKG_NAME} gen-completion --shell fish --out-dir ${PREFIX}/share/fish/vendor_completions.d/${PKG_NAME}.fish
 fi
-
-mkdir -p ${PREFIX}/etc/bash_completion.d/${PKG_NAME}
-mkdir -p ${PREFIX}/share/zsh/site-functions/_${PKG_NAME}
-mkdir -p ${PREFIX}/share/fish/vendor_completions.d/${PKG_NAME}.fish
-${atuin_path} gen-completion --shell bash --out-dir ${PREFIX}/etc/bash_completion.d/${PKG_NAME}
-${atuin_path} gen-completion --shell zsh --out-dir ${PREFIX}/share/zsh/site-functions/_${PKG_NAME}
-${atuin_path} gen-completion --shell fish --out-dir ${PREFIX}/share/fish/vendor_completions.d/${PKG_NAME}.fish
 
 # remove extra build file
 rm -f "${PREFIX}/.crates.toml"
