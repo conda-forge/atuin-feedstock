9c01eee924b03d095bf6d7957eba48c42352c975
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-01 17:47:20 -0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,6 @@ cargo-bundle-licenses \
     --format yaml \
     --output THIRDPARTY.yml
 
-export PROTOC=${BUILD_PREFIX}/bin/protoc
 cargo install --bins --no-track --locked --root "${PREFIX}" --path crates/atuin
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
